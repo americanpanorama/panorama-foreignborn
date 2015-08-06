1d4ed9d64dbdd7083df0ff72d4a8197b7b8e530a
Sean Connelley <connelley@stamen.com>
2015-08-06 10:12:07 -0400

    Initial store to put text for app

---
+++ app/stores/foreignbornCopy.js
@@ -0,0 +1,13 @@
+//
+// Text for About area in Foreign Born
+//
+//
+
+var ForeignBornCopy = {};
+
+ForeignBornCopy.years = {};
+ForeignBornCopy.years['1850'] = "";
+//...
+
+
+module.exports = ForeignBornCopy;
