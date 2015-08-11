1e381f53d710c4e373801bde0a09f4607b9d5861
Sean Connelley <connelley@stamen.com>
2015-08-11 16:00:34 -0400

    Encoding space in hash with "+" sign, #131

--- app/lib/hashManager.js
+++ app/lib/hashManager.js
@@ -17,10 +17,14 @@ var hashManager = {
       hash = hash.substr(1);
     }
 
+    hash = this.decode(hash);
+
     var that = this;
     var things = hash.split('&').map(function(d){return d.split('=');});
 
     things.forEach(function(thing){
+      thing[0] = that.decode(thing[0]);
+      thing[1] = that.decode(thing[1]);
       if (thing[0] in that.hashParams && thing[1] != '') {
         out[thing[0]] = thing[1];
       }
@@ -46,6 +50,14 @@ var hashManager = {
     return this.hashParams;
   },
 
+  encode: function(item) {
+    return item.replace(/ /g, '+');
+  },
+
+  decode: function(item) {
+    return item.replace(/\+/g, ' ');
+  },
+
   updateHash: function(silent) {
     var out = [];
 
@@ -57,7 +69,8 @@ var hashManager = {
       }
     }
 
-    var hash = "#" + out.join('&');
+    var hash =  this.encode("#" + out.join('&'));
+
     if (document.location.hash !== hash) document.location.replace(hash);
   }
 }
