9ee065713a66f187b8ce9fbf66937cec27ff0028
Sean Connelley <connelley@stamen.com>
2015-12-14 21:46:18 -0500

    Fixed Panorama Navigation modal styles

--- app/components/PanoramaNavigation.jsx
+++ app/components/PanoramaNavigation.jsx
@@ -14,7 +14,7 @@ var PanoramaNavigation = React.createClass({
 	},
 
 	componentWillMount: function() {
-	
+
 	},
 
 	componentDidUpdate: function() {
@@ -35,21 +35,22 @@ var PanoramaNavigation = React.createClass({
 			<div>
 
 				<div id='hamburger'><img src='http://dsl.richmond.edu/panorama/static/images/hamburger.png' onClick={ this.props.on_hamburger_click } /></div>
-	
+
 				<Modal
 				  isOpen={ this.props.show_panorama_menu }
 				  onRequestClose={ this.props.on_hamburger_click }
+				  overlayClassName={'panorama-modal'}
 				  className="nav_header"
 				>
 					<div id='navburger'><img src='http://dsl.richmond.edu/panorama/static/images/hamburger.png' onClick={ this.props.on_hamburger_click } /></div>
-	
+
 					<div id='nav_header'>
 						<h1><a href='/panorama/'>American Panorama</a></h1>
 						{
 							this.props.nav_data.map(function(item, i) {
 								return (
 									<div className='pan_nav_item' style={{width: that.computeDimensions() + 'px'}}>
-										<img src={ item.screenshot } style={{width: that.computeDimensions() + 'px'}} /><br/> 
+										<img src={ item.screenshot } style={{width: that.computeDimensions() + 'px'}} /><br/>
 										<h4>
 											<a href='{ this.url }'>{ item.title }</a>
 										</h4>
